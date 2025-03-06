952999e2f02e5010fb074aabacaf56c57b8ed6a7
Gkzgn-21 <gulsum.kizgin@std.medipol.edu.tr>
2025-03-06 15:15:23 +0300

    abstractodev

---
+++ Abstrackt/Abstrackt/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+       
+
+
+        public interface IVehicle
+        {
+            string Brand { get; }
+            double FuelConsumption { get; } 
+            double CalculateTotalConsumption(int hours); 
+        }
+
+       
+        public abstract class VehicleBase : IVehicle
+        {
+            public abstract string Brand { get; }
+            public abstract double FuelConsumption { get; }
+
+           
+            public double CalculateTotalConsumption(int hours)
+            {
+                return FuelConsumption * hours;
+            }
+        }
+
+  
+        public class BMW : VehicleBase
+        {
+            public override string Brand => "BMW";
+            public override double FuelConsumption => 80.0; 
+        }
+
+   
+        public class Mercedes : VehicleBase
+        {
+            public override string Brand => "Mercedes";
+            public override double FuelConsumption => 70.0; 
+        }
+
+     
+        public class Porsche : VehicleBase
+        {
+            public override string Brand => "Porsche";
+            public override double FuelConsumption => 60.0; 
+        }
+
+        public class Progra
+        {
+            public static void Main()
+            {
+               
+                List<IVehicle> vehicles = new List<IVehicle>
+        {
+            new BMW(),
+            new Mercedes(),
+            new Porsche()
+        };
+
+              
+                double totalConsumption = 0;
+                int hoursDriven = 1;
+
+                foreach (var vehicle in vehicles)
+                {
+                    double consumption = vehicle.CalculateTotalConsumption(hoursDriven);
+                    Console.WriteLine($"{vehicle.Brand} için tüketim: {consumption} L");
+                    totalConsumption += consumption;
+                }
+
+                Console.WriteLine($"Toplam tüketim: {totalConsumption} L");
+            }
+
+        }
+    }
+}
---
+++ abstract_car/abstract_car/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_car
+{
+    internal class Program
+    {
+       
+       
+            // 1. Araçların temel özelliklerini içeren interface
+public interface IVehicle
+        {
+            string Brand { get; }  // Araç Markası
+            void Drive();  // Karada sürüş
+            void Float();  // Denizde yüzme
+            void Fly();    // Havada uçma
+        }
+
+
+        public abstract class VehicleBase : IVehicle
+        {
+            public abstract string Brand { get; }
+
+     
+            public virtual void Drive()
+            {
+                Console.WriteLine($"{Brand} karada çok hızlı gider.");
+            }
+
+            public abstract void Float(); 
+            public abstract void Fly();  
+        }
+
+        
+        public class BMW : VehicleBase
+        {
+            public override string Brand => "BMW";
+
+            public override void Float()
+            {
+                Console.WriteLine($"{Brand} denizde yüzer.");
+            }
+
+            public override void Fly()
+            {
+                Console.WriteLine($"{Brand} havada uçar.");
+            }
+        }
+
+        public class Mercedes : VehicleBase
+        {
+            public override string Brand => "Mercedes";
+
+            public override void Float()
+            {
+                Console.WriteLine($"{Brand} denizde yüzer.");
+            }
+
+            public override void Fly()
+            {
+                Console.WriteLine($"{Brand} havada uçar.");
+            }
+
+            public override void Drive()
+            {
+                Console.WriteLine($"{Brand} karada güvenli bir şekilde gider.");
+            }
+        }
+
+
+        public class Porsche : VehicleBase
+        {
+            public override string Brand => "Porsche";
+
+            public override void Float()
+            {
+                Console.WriteLine($"{Brand} denizde yüzemez.");
+            }
+
+            public override void Fly()
+            {
+                Console.WriteLine($"{Brand} havada uçar.");
+            }
+        }
+
+        public class Prog
+        {
+            public static void Main()
+            {
+                List<IVehicle> vehicles = new List<IVehicle>
+        {
+            new BMW(),
+            new Mercedes(),
+            new Porsche()
+        };
+
+              
+                foreach (var vehicle in vehicles)
+                {
+                    Console.WriteLine($"\n{vehicle.Brand} özellikleri:");
+                    vehicle.Drive();
+                    vehicle.Float();
+                    vehicle.Fly();
+                }
+                 }
+
+            }
+        }
+}
