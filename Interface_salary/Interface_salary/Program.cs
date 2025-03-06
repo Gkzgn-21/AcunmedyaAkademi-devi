a61815c09580ce42bcf5c11698ecf9cc44676009
Gkzgn-21 <gulsum.kizgin@std.medipol.edu.tr>
2025-03-06 13:25:55 +0300

    abstractödev3

---
+++ Interface_salary/Interface_salary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_salary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                Dictionary<string, double> calisanMaaslari = new Dictionary<string, double>()
+        {
+            { "GenelMüdür", 80000.0 },
+            { "Müdür", 60000.0 },
+            { "Programci", 40000.0 },
+            { "Stajyer", 5000.0 }
+        };
+
+                double toplamMaas = calisanMaaslari.Values.Sum();
+
+                Console.WriteLine("Toplam maaş: " + toplamMaas);
+            }
+        }
+
+    }
+}
+
+
---
+++ Interface_salary/Interface_salary/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Interface_salary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interface_salary")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2467dda4-e984-48d8-b1de-2e21d97ad1d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
